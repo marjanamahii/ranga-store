eca48158a341270aaf9c3e39b9aae064b23e7387
marjanamahii <marjanamahi777@gmail.com>
2021-09-13 23:15:08 +0600

    button color update

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ const showProducts = (products) => {
           <small>Total Ratings Count: ${product.rating.count}</small>
           <h2>Price: $ ${product.price}</h2>
           <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info">add to cart</button>
-          <button id="details-btn" class="btn btn-warning">Details</button>  
+          <button id="details-btn" class="btn btn-danger">Details</button>  
       </div>
       </div>
       `;
